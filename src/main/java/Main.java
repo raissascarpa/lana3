eb993a444955771368b3f55f562e15130c40adae
raissascarpa <162500353+raissascarpa@users.noreply.github.com>
2024-03-19 14:18:29 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+// import org.junit.jupiter.api.Test;
+//olá mundo em java  
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("olá mundo!");
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
